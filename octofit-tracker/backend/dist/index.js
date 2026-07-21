8e43eaad2c07fd5db314a7859ebb5aba109ba949
Rihem <rihem.ing@gmail.com>
2026-07-21 12:33:10 +0000

    Wire API hosting for the multi-tier application

--- octofit-tracker/backend/dist/index.js
+++ octofit-tracker/backend/dist/index.js
@@ -1,28 +1,3 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
-const dotenv_1 = __importDefault(require("dotenv"));
-require("./config/database");
-const users_1 = __importDefault(require("./routes/users"));
-const teams_1 = __importDefault(require("./routes/teams"));
-const activities_1 = __importDefault(require("./routes/activities"));
-const leaderboard_1 = __importDefault(require("./routes/leaderboard"));
-const workouts_1 = __importDefault(require("./routes/workouts"));
-dotenv_1.default.config();
-const app = (0, express_1.default)();
-const port = Number(process.env.PORT) || 8000;
-app.use(express_1.default.json());
-app.get('/api/health', (_req, res) => {
-    res.json({ status: 'ok', message: 'OctoFit Tracker API is running' });
-});
-app.use('/api/users', users_1.default);
-app.use('/api/teams', teams_1.default);
-app.use('/api/activities', activities_1.default);
-app.use('/api/leaderboard', leaderboard_1.default);
-app.use('/api/workouts', workouts_1.default);
-app.listen(port, () => {
-    console.log(`OctoFit Tracker backend listening on port ${port}`);
-});
+require("./server");
